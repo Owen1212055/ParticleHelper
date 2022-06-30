cf73c66ad4671372cf7dea42ba2e1359530f9178
Owen <23108066+Owen1212055@users.noreply.github.com>
2022-06-30 14:12:08 -0400

    Cleanup

--- nms/build.gradle.kts
+++ nms/build.gradle.kts
@@ -1,6 +1,3 @@
-import java.nio.file.Files
-import java.nio.file.StandardCopyOption
-
 plugins {
     java
     id("io.papermc.paperweight.userdev") version "1.3.5"
@@ -25,20 +22,4 @@ dependencies {
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-val moveJars by tasks.register("moveFiles", DefaultTask::class)
-
-moveJars.doFirst {
-
-    val file = tasks.named<io.papermc.paperweight.tasks.RemapJar>("reobfJar").flatMap { task -> task.outputJar }
-        .get().asFile.toPath()
-
-    if (Files.exists(file)) {
-        Files.move(
-            file,
-            project(":plugin").projectDir.toPath().resolve("src/main/resources/nms-1.0.jar"),
-            StandardCopyOption.REPLACE_EXISTING
-        )
-    }
 }
