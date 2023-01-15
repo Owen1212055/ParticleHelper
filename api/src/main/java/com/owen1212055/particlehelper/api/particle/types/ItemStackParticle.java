1b9d2394357f6d2366df92704f3fe052bc061b4d
Owen <23108066+Owen1212055@users.noreply.github.com>
2023-01-15 00:24:17 -0500

    Material -> ItemStack for icon data

--- api/src/main/java/com/owen1212055/particlehelper/api/particle/types/ItemStackParticle.java
+++ api/src/main/java/com/owen1212055/particlehelper/api/particle/types/ItemStackParticle.java
@@ -1,17 +1,17 @@
 package com.owen1212055.particlehelper.api.particle.types;
 
 import com.owen1212055.particlehelper.api.particle.Particle;
-import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a particle which can have the material it represents
  * modified.
  */
-public interface MaterialParticle extends Particle {
+public interface ItemStackParticle extends Particle {
 
     @NotNull
-    Material getMaterial();
+    ItemStack getItemStack();
 
-    void setMaterial(@NotNull Material material);
+    void setItemStack(@NotNull ItemStack material);
 }
--- api/src/main/java/com/owen1212055/particlehelper/api/particle/types/itemcrack/MultiItemCrackParticle.java
+++ api/src/main/java/com/owen1212055/particlehelper/api/particle/types/itemcrack/MultiItemCrackParticle.java
@@ -2,27 +2,28 @@ package com.owen1212055.particlehelper.api.particle.types.itemcrack;
 
 import com.owen1212055.particlehelper.api.particle.compiled.CompiledParticle;
 import com.owen1212055.particlehelper.api.particle.compiled.simple.SimpleCompiledParticle;
-import com.owen1212055.particlehelper.api.particle.types.MaterialParticle;
+import com.owen1212055.particlehelper.api.particle.types.ItemStackParticle;
 import com.owen1212055.particlehelper.api.particle.types.common.velocity.MultiSpeedModifiableParticle;
 import com.owen1212055.particlehelper.api.type.ParticleType;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class MultiItemCrackParticle extends MultiSpeedModifiableParticle implements MaterialParticle {
+public class MultiItemCrackParticle extends MultiSpeedModifiableParticle implements ItemStackParticle {
 
-    private Material data = Material.APPLE;
+    private ItemStack data = new ItemStack(Material.APPLE);
 
     public MultiItemCrackParticle(ParticleType<?, ?> particleType) {
         super(particleType);
     }
 
     @Override
-    public @NotNull Material getMaterial() {
+    public @NotNull ItemStack getItemStack() {
         return this.data;
     }
 
     @Override
-    public void setMaterial(@NotNull Material material) {
+    public void setItemStack(@NotNull ItemStack material) {
         this.data = material;
     }
 
